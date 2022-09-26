51dbfe7e458622b2fb2b6072dc3c7657ea26171d
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-25 21:51:33 -0300

    labis

--- libs/libs-alerta/js/contato.js
+++ libs/libs-alerta/js/contato.js
@@ -1,7 +1,10 @@
-const btn = document.querySelector("#btn");
-const nome = document.querySelector("#nome");
+const btn = document.querySelector("#btn"); // pegando id
+const nome = document.querySelector("#nome"); // pegando id
 toastr.options.positionClass = "toast-bottom-left";
 
+
+// Quando eu apertar no enviar, se o campo nome tiver vazio, vau  dá erro.
+// Se o campo nome tiver preencido, vai dá sucesso
 btn.onclick = () => {
     if(!nome.value){
         return toastr.error('Dados incorretos');
