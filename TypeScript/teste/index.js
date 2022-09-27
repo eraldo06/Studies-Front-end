a67626a9eb92c273072723d5473c922c50f69f13
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-27 11:22:46 -0300

    typeScript

---
+++ TypeScript/teste/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function somar(n1, n2) {
+    return n1 + n2;
+}
+const n1 = 10;
+const n2 = '20';
+console.log(somar(n1, n2));
---
+++ TypeScript/teste/index.ts
@@ -0,0 +1,6 @@
+function somar(n1:number, n2:number){
+    return n1 + n2;
+}
+const n1 = 10;
+const n2 = 20;
+console.log(somar(n1,n2));
