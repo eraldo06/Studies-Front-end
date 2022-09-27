c71a7142496f878ed6449cc63e270b53af650081
eraldo06 <eraldoleonardo384@gmail.com>
2022-09-27 11:27:57 -0300

    typeScript

---
+++ TypeScript/aula02/index.ts
@@ -0,0 +1,17 @@
+// Boolean
+const contapaga: boolean = false;
+
+// Number
+const idade: number = 23;
+const avalicao: number = 4.5;
+
+// String
+const nome: string = 'Eraldo Leonardo';
+
+// Array
+const idades: number[] = [23,20,21,19];
+const idades2: Array<number> = [23,20,21,19];
+
+// Tuple
+let jogadores: [string, string, string];
+jogadores = ['Eraldo', 'Leonardo', 'Paulino']
