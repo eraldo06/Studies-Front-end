c002045099d020eedb81bfadfb36bc8d7de61ad2
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-02 16:11:13 -0300

    react

--- React/aula01/gama-react/src/Routes.tsx
+++ React/aula01/gama-react/src/Routes.tsx
@@ -1,3 +1,4 @@
+
 import {Routes as WrapperRoutes, Route, BrowserRouter} from "react-router-dom";
 import Contato from "./pages/Contato";
 import Home from "./pages/Home";
@@ -5,7 +6,7 @@ export default function Routes(){
     return(
     <BrowserRouter>
     <WrapperRoutes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/:nome" element={<Home/>} />
         <Route path="/contato" element={<Contato/>} />
     </WrapperRoutes>
     </BrowserRouter>
--- React/aula01/gama-react/src/pages/Home/index.tsx
+++ React/aula01/gama-react/src/pages/Home/index.tsx
@@ -1,21 +1,24 @@
 import Header from "../../componetes/Header";
 import Box from "../../componetes/Box"
 import Link from "../../componetes/Link";
+import {useParams} from "react-router-dom";
 import "../../style/global.css"
 
 function Home() {
+    const params = useParams();
   return (
     
     <div className="App">
       <Header/>
       <h1>Pagina Home</h1>
+      <h2>Olá {params.nome}</h2>
       <main>
         <Box background="claro">
             <h1>teste</h1>
         </Box>
         <Box background="escuro">
             <h1>teste</h1>
-          </Box> 
+        </Box> 
       </main>
     </div>
   );
