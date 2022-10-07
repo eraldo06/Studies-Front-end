c32e0c67f93555ab54cb5e5daa68d77423dcdc61
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-06 22:33:52 -0300

    react

--- React/aula02/src/App.tsx
+++ React/aula02/src/App.tsx
@@ -1,9 +1,10 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import TaskContainer from "./componets/TaskContainer";
 import TaskItem from "./componets/TakItem";
 import "./style/estilo.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 
+// interface da tarefa
 interface Tarefa{
     titulo: string;
     done: boolean;
@@ -15,12 +16,43 @@ function App() {
     done: false,
   }]);
 
+
+// tarefa
   function adicionarTarefa(){
-    setTarefas([...tarefas, {
-      titulo: 'tarefa 2',
-      done: false
-    }])
+    if(tarefas.length<=4){
+      setTarefas([...tarefas, {
+        titulo: 'tarefa 2',
+        done: false
+      }])
+    }
+   
   }
+
+// brincando
+  const na = [<button>1</button>, <button>2</button>,<button>3</button>  ] 
+
+  // adicionar uma tarefa
+  function concluirTarefa(posi:number){
+    const novaLista = [...tarefas];
+    novaLista[posi].done = true;
+    setTarefas(novaLista);
+  }
+
+  // remover tarefa
+  function removerTarefa(posi:number){
+    const nova2Lista = [...tarefas];
+    nova2Lista[posi].done = false;
+    setTarefas(nova2Lista);
+  }
+
+  // alerta de tarefa
+  useEffect(
+    ()=>{
+      if (tarefas.length>=5){
+        alert('LIMITE')
+      }
+    },
+    [tarefas])
 
   return (
     <div className="App">
@@ -30,10 +62,20 @@ function App() {
           <button id="btnAdd" className="btn btn-primary" onClick={adicionarTarefa}>Add</button>
         </div>
         <TaskContainer>
-        {tarefas.map((tarefa: Tarefa)=>{
-          return<TaskItem titulo={tarefa.titulo} done={tarefa.done}/>
+        {tarefas.map((tarefa: Tarefa, posi:number)=>{
+          return<TaskItem 
+          titulo={tarefa.titulo} 
+          done={tarefa.done} 
+          concluirTarefa={()=>concluirTarefa(posi)}
+          removerTarefa={()=>removerTarefa(posi)}
+          />
         })}
         </TaskContainer>
+
+        {/* brincando */}
+        <div>
+        {na[2]}
+        </div>
        
       </main>
     </div>
--- React/aula02/src/componets/TakItem/index.tsx
+++ React/aula02/src/componets/TakItem/index.tsx
@@ -1,13 +1,15 @@
 interface TaskItemProps{
     titulo: string;
     done?: boolean; // ? quer dizer opcional
+    concluirTarefa: ()=>void;
+    removerTarefa: ()=>any;
 }
 export default function TaskItem(props: TaskItemProps){
     return(
         <li className={`${props.done ? "done" : ""}`}>
-            <span><strong>X</strong></span>
+            <span onClick={props.removerTarefa} ><strong>X</strong></span>
             <p>{props.titulo}</p>
-            <span><strong>V</strong></span>
+            <span onClick={props.concluirTarefa}><strong>V</strong></span>
         </li>
     )
 }
