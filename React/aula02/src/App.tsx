d1f1788b4f858ee4ad346053946d4ebf0f1dbb7b
eraldo06 <eraldoleonardo384@gmail.com>
2023-01-11 20:05:25 -0300

    praticando sobre props no react

--- React/aula02/src/App.tsx
+++ React/aula02/src/App.tsx
@@ -1,85 +1,17 @@
-import {useState, useEffect} from "react";
-import TaskContainer from "./componets/TaskContainer";
-import TaskItem from "./componets/TakItem";
-import "./style/estilo.css";
-import "bootstrap/dist/css/bootstrap.min.css"
-
-// interface da tarefa
-interface Tarefa{
-    titulo: string;
-    done: boolean;
-}
+import { Foto } from "./componentes/Foto";
 
 function App() {
-  const [tarefas, setTarefas] = useState<Tarefa[]>([{
-    titulo: "tarefa",
-    done: false,
-  }]);
-
-
-// tarefa
-  function adicionarTarefa(){
-    if(tarefas.length<=4){
-      setTarefas([...tarefas, {
-        titulo: 'tarefa 2',
-        done: false
-      }])
-    }
-   
-  }
-
-// brincando
-  const na = [<button>1</button>, <button>2</button>,<button>3</button>  ] 
-
-  // adicionar uma tarefa
-  function concluirTarefa(posi:number){
-    const novaLista = [...tarefas];
-    novaLista[posi].done = true;
-    setTarefas(novaLista);
-  }
-
-  // remover tarefa
-  function removerTarefa(posi:number){
-    const nova2Lista = [...tarefas];
-    nova2Lista[posi].done = false;
-    setTarefas(nova2Lista);
-  }
-
-  // alerta de tarefa
-  useEffect(
-    ()=>{
-      if (tarefas.length>=5){
-        alert('LIMITE')
-      }
-    },
-    [tarefas])
-
-  return (
-    <div className="App">
-      <main className="container">
-        <h1 className="m-5">Task Manager</h1>
-        <div id="new-task">
-          <button id="btnAdd" className="btn btn-primary" onClick={adicionarTarefa}>Add</button>
-        </div>
-        <TaskContainer>
-        {tarefas.map((tarefa: Tarefa, posi:number)=>{
-          return<TaskItem 
-          titulo={tarefa.titulo} 
-          done={tarefa.done} 
-          concluirTarefa={()=>concluirTarefa(posi)}
-          removerTarefa={()=>removerTarefa(posi)}
-          />
-        })}
-        </TaskContainer>
-
-        {/* brincando */}
-        <div>
-        {na[2]}
-        </div>
-       
-      </main>
-    </div>
-  );
+  return(
+   <>
+       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quibusdam? Nesciunt iure vitae obcaecati est, earum explicabo quo quaerat vero quam. Molestias fuga ipsam quo dignissimos quam laboriosam magni impedit.</h1>
+       <hr />
+       <Foto url='https://www.google.com/logos/google.jpg'>
+         <img src="https://www.google.com/logos/google.jpg" alt="" />
+         <p>dois</p>cxczxcxz
+       </Foto>
+   </>
+    
+  )
 }
 
 export default App;
---
+++ React/aula02/src/componentes/Foto.tsx
@@ -0,0 +1,18 @@
+import { Children, ReactNode } from "react"
+
+type Props = {
+    url: string;
+    children: ReactNode;
+}
+
+export const Foto = ({url}: Props)=>{
+    return(
+        <>
+               
+          
+           <p>pegpoi</p>
+          
+           
+        </>
+    )
+}
